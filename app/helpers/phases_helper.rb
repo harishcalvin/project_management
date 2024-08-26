b1bbe9cea47f01e6ab55e72221c4ed922057057d
harish <hari991996@gmail.com>
2024-08-26 21:16:21 +0530

    add phases ui and filter total number

--- app/helpers/phases_helper.rb
+++ app/helpers/phases_helper.rb
@@ -1,2 +1,14 @@
 module PhasesHelper
+  def status_color(status)
+    case status
+    when 'pending'
+      'bg-yellow-100 text-yellow-800'
+    when 'in_progress'
+      'bg-blue-100 text-blue-800'
+    when 'completed'
+      'bg-green-100 text-green-800'
+    else
+      'bg-gray-100 text-gray-800'
+    end
+  end
 end
